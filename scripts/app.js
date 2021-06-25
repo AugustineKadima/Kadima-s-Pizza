bb9dc7307754ebeff20c6bc3e8c41848a12fc1ed
C0d3president <kadimasam@gmail.com>
2021-06-25 23:46:22 +0300

    Use jquery to select dom elements

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+
+
+
+
+$(document).ready(function(){
+$("button#customizeOrder").click(function(event){
+
+    event.preventDefault();
+
+    let typeOfPizza = $("#pizzaTypes").val();
+    let pizzaSize = $("#pizzaSize").val();
+    let crust = $("#crust").val();
+    let toppings = $("#toppings").val();
+    let deliveryOption = $("input[name='delivery']").val()
+
+    
+});
+});
