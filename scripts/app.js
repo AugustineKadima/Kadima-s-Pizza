6bd014d3ec5b2c0b7cc76d4e5aa9896aee0f81ee
C0d3president <kadimasam@gmail.com>
2021-06-26 01:09:52 +0300

    Add a constructor for customer order

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,15 @@
 
+let customerOrder = function(typeOfPizza, pizzaSize, crust,toppings, deliveryOption){
 
+    this.typeOfPizza = typeOfPizza;
+    this.pizzaSize = pizzaSize;
+    this.crust = crust;
+    this.toppings = toppings;
+    this.deliveryOption = deliveryOption;
+    let orderArray = [this.typeOfPizza, this.pizzaSize, this.crust, this.toppings, this.deliveryOption];
+    
+    
+}
 
 
 $(document).ready(function(){
