593ed532fb4527ba70d09a3e29375719bb0b18f4
C0d3president <kadimasam@gmail.com>
2021-06-26 02:49:13 +0300

    Add checkout button and form fields

--- scripts/app.js
+++ scripts/app.js
@@ -10,6 +10,8 @@ let CustomerOrder = function(typeOfPizza, pizzaSize, crust,toppings, deliveryOpt
     
 }
 
+// PIZZA PRICE OBJECT
+
 
 $(document).ready(function(){
 $("button#customizeOrder").click(function(event){
@@ -23,7 +25,7 @@ $("button#customizeOrder").click(function(event){
     let deliveryOption = $("input[name='delivery']").val()
 
     // NEW INSTANCE OF CUSTOMER OBJECT
-    let customerOrder = new CustomerOrder(typeOfPizza, pizzaSize, crust,toppings, deliveryOption);
+    let customerOrder = new CustomerOrder(typeOfPizza, pizzaSize, crust, toppings, deliveryOption);
 
     // OUTPUT
     $(".formOutput").text(customerOrder.typeOfPizza);
