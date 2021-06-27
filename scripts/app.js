37fe14b17e182a11d9a30fa97bafbb01f41e6023
C0d3president <kadimasam@gmail.com>
2021-06-27 03:11:22 +0300

    Add a pricing object

--- scripts/app.js
+++ scripts/app.js
@@ -11,13 +11,12 @@ let CustomerOrder = function(typeOfPizza, pizzaSize, crust,toppings, deliveryOpt
     this.deliveryLocation = deliveryLocation;
 }
 
-
-
-// KADIMA'S PIZZA
-let kadimasPizza = { 
+// PRICING OBJECT
+CustomerOrder.prototype.kadimasPizza = { 
     name: "Kadima's Pizza",
     pizzaSizePrice: [{small: 500}, {medium: 800}, {large: 1200}],
-    pizzaCrustPrice: [{Crispy: 200}, {Stuffed: 220}, {Glutten_free: 300}]
+    pizzaCrustPrice: [{Crispy: 200}, {Stuffed: 220}, {Glutten_free: 300}],
+    deliveryPrice: 100
 }
 
 
@@ -33,13 +32,13 @@ $("button#customizeOrder").click(function(event){
     let deliveryOption = $("input[name='delivery']").val()
     let pizzaNumberTotal = parseInt($("#pizzaNumberTotal").val());
     let deliveryLocation = $("#location").val();
-    let deliveryPrice = 100;
+    
 
     // NEW INSTANCE OF CUSTOMER OBJECT
     let customerOrder = new CustomerOrder(typeOfPizza, pizzaSize, crust, toppings, deliveryOption, pizzaNumberTotal, deliveryLocation );
 
     // OUTPUT
-    $(".formOutput").text(customerOrder.typeOfPizza);
+    $(".formOutput").text(customerOrder.kadimasPizza.pizzaCrustPrice[0].Crispy);
 
     
 });
