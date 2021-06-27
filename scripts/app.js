1a222ec595220e341722b914e8edff91eccd6d78
C0d3president <kadimasam@gmail.com>
2021-06-27 21:04:57 +0300

    Add large options

--- scripts/app.js
+++ scripts/app.js
@@ -228,6 +228,93 @@ $("button#customizeOrder").click(function(event){
         $(".formOutput").text(totalPrice);
     }
 
+
+     // PRICE CALCULATIONS
+    // For LARGE pizza
+
+
+    // CRISPY option - potato_sausage_bacon
+    if (pizzaSize == "large" && crust == "crispy" && toppings == "potato_sausage_bacon" && deliveryOption == "on"){
+        let totalPrice = (customerOrder.kadimasPizza.pizzaSizePrice[3].large + 
+            customerOrder.kadimasPizza.pizzaCrustPrice[1].crispy + 
+            customerOrder.kadimasPizza.toppingPrice[1].potato_sausage_bacon + 
+            customerOrder.kadimasPizza.deliveryPrice) * pizzaNumberTotal;
+        $(".formOutput").text(totalPrice);
+    }
+
+    // crispy black_olives_anchovies_capers
+    if (pizzaSize == "large" && crust == "crispy" && toppings == "black_olives_anchovies_capers" && deliveryOption == "on"){
+        let totalPrice = (customerOrder.kadimasPizza.pizzaSizePrice[3].large + 
+            customerOrder.kadimasPizza.pizzaCrustPrice[1].crispy + 
+            customerOrder.kadimasPizza.toppingPrice[2].black_olives_anchovies_capers + 
+            customerOrder.kadimasPizza.deliveryPrice) * pizzaNumberTotal;
+        $(".formOutput").text(totalPrice);
+    }
+
+     // crispy mixed_sliced_mushrooms_and_garlic
+     if (pizzaSize == "large" && crust == "crispy" && toppings == "mixed_sliced_mushrooms_and_garlic" && deliveryOption == "on"){
+        let totalPrice = (customerOrder.kadimasPizza.pizzaSizePrice[3].large + 
+            customerOrder.kadimasPizza.pizzaCrustPrice[1].crispy + 
+            customerOrder.kadimasPizza.toppingPrice[3].mixed_sliced_mushrooms_and_garlic + 
+            customerOrder.kadimasPizza.deliveryPrice) * pizzaNumberTotal;
+        $(".formOutput").text(totalPrice);
+    }
+
+
+    // STUFFED option - potato_sausage_bacon
+    if (pizzaSize == "large" && crust == "stuffed" && toppings == "potato_sausage_bacon" && deliveryOption == "on"){
+        let totalPrice = (customerOrder.kadimasPizza.pizzaSizePrice[3].large + 
+            customerOrder.kadimasPizza.pizzaCrustPrice[2].stuffed + 
+            customerOrder.kadimasPizza.toppingPrice[1].potato_sausage_bacon + 
+            customerOrder.kadimasPizza.deliveryPrice) * pizzaNumberTotal;
+        $(".formOutput").text(totalPrice);
+    }
+
+     // STUFFED option - black_olives_anchovies_capers
+     if (pizzaSize == "large" && crust == "stuffed" && toppings == "black_olives_anchovies_capers" && deliveryOption == "on"){
+        let totalPrice = (customerOrder.kadimasPizza.pizzaSizePrice[3].large + 
+            customerOrder.kadimasPizza.pizzaCrustPrice[2].stuffed + 
+            customerOrder.kadimasPizza.toppingPrice[2].black_olives_anchovies_capers + 
+            customerOrder.kadimasPizza.deliveryPrice) * pizzaNumberTotal;
+        $(".formOutput").text(totalPrice);
+    }
+
+     // STUFFED option - mixed_sliced_mushrooms_and_garlic
+     if (pizzaSize == "large" && crust == "stuffed" && toppings == "mixed_sliced_mushrooms_and_garlic" && deliveryOption == "on"){
+        let totalPrice = (customerOrder.kadimasPizza.pizzaSizePrice[3].large + 
+            customerOrder.kadimasPizza.pizzaCrustPrice[2].stuffed + 
+            customerOrder.kadimasPizza.toppingPrice[3].mixed_sliced_mushrooms_and_garlic + 
+            customerOrder.kadimasPizza.deliveryPrice) * pizzaNumberTotal;
+        $(".formOutput").text(totalPrice);
+    }
+
+    // GLUTTEN_FREE option - potato_sausage_bacon
+    if (pizzaSize == "large" && crust == "glutten_free" && toppings == "potato_sausage_bacon" && deliveryOption == "on"){
+        let totalPrice = (customerOrder.kadimasPizza.pizzaSizePrice[3].large + 
+            customerOrder.kadimasPizza.pizzaCrustPrice[3].glutten_free + 
+            customerOrder.kadimasPizza.toppingPrice[1].potato_sausage_bacon + 
+            customerOrder.kadimasPizza.deliveryPrice) * pizzaNumberTotal;
+        $(".formOutput").text(totalPrice);
+    }
+
+    // GLUTTEN_FREE option - black_olives_anchovies_capers
+    if (pizzaSize == "large" && crust == "glutten_free" && toppings == "black_olives_anchovies_capers" && deliveryOption == "on"){
+        let totalPrice = (customerOrder.kadimasPizza.pizzaSizePrice[3].large + 
+            customerOrder.kadimasPizza.pizzaCrustPrice[3].glutten_free + 
+            customerOrder.kadimasPizza.toppingPrice[2].black_olives_anchovies_capers + 
+            customerOrder.kadimasPizza.deliveryPrice) * pizzaNumberTotal;
+        $(".formOutput").text(totalPrice);
+    }
+
+     // GLUTTEN_FREE option - mixed_sliced_mushrooms_and_garlic
+     if (pizzaSize == "large" && crust == "glutten_free" && toppings == "mixed_sliced_mushrooms_and_garlic" && deliveryOption == "on"){
+        let totalPrice = (customerOrder.kadimasPizza.pizzaSizePrice[3].large + 
+            customerOrder.kadimasPizza.pizzaCrustPrice[3].glutten_free + 
+            customerOrder.kadimasPizza.toppingPrice[3].mixed_sliced_mushrooms_and_garlic + 
+            customerOrder.kadimasPizza.deliveryPrice) * pizzaNumberTotal;
+        $(".formOutput").text(totalPrice);
+    }
+
     
 
     
